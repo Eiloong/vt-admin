92737a33d4f6db9327f40441ccff16c1d0dc575f
onlyoneLong <1543687061@qq.com>
2022-08-15 15:05:50 +0800

    feat：新增prettier规则

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 120, // 超过 120 字符换行
+  tabWidth: 2, // 缩进空格数
+  semi: false, // 结尾使用分号（true 使用，false 不使用）
+  singleQuote: true, // 使用单引号（true 单引号，false 双引号）
+  jsxSingleQuote: false, // 在 jsx 中使用单引号（true 单引号，false 双引号）
+  trailingComma: 'none' // 是否使用尾逗号（none 不使用，es5 全部使用，all 全部使用，es2015 除开最后一个）
+}
