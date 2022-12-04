41ece8ec2e04c8219938410b2151417cbdf3d0dc
onlyoneLong <1543687061@qq.com>
2022-12-04 10:29:20 +0800

    chore: 新增饿了么图标

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,14 @@ import './styles/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-// createApp(App).use(router).use(ElementPlus).mount('#app')
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 async function bootstrap() {
   const app = createApp(App)
+
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
   setupRouter(app)
   setupStore(app)
   app.use(ElementPlus, { i18n: i18n.global.t }).use(i18n)
